8862af6a0ea852a3a16909f311e85a6ef01e96c8
Saad Al-Marsumi <saad.almarsumi@orthogonal.io>
2024-05-03 03:48:01 +0300

    Refactor C++ client for better error handling.

--- src/COMCalculator.NativeClient/COMCalculator.NativeClient.cpp
+++ src/COMCalculator.NativeClient/COMCalculator.NativeClient.cpp
@@ -6,17 +6,22 @@
 #include "EventSink.h"
 #include "COMCalculator.h"
 #include <string>
-//#import "COMCalculator.tlb" no_namespace, raw_interfaces_only
 
 using namespace ATL;
 
+int ExitWithError(HRESULT hr, std::string message)
+{
+    std::cerr << message << std::system_category().message(hr) << std::endl;
+    CoUninitialize();
+    return -1;
+}
+
 int main()
 {
-    std::cout << "Visual C++ Native Client\n";
+    std::cout << "Visual C++ Client\n";
 
     HRESULT hr;
     long x = 10, y = 5;
-    //ICalculator* pCalc = NULL;
     long result;
 
     CComPtr<ICalculator> pCalc;
@@ -26,49 +31,45 @@ int main()
 
     // Create an instance of the Calculator object.
     hr = pCalc.CoCreateInstance(__uuidof(Calculator));
-    //hr = CoCreateInstance(__uuidof(Calculator), NULL, CLSCTX_INPROC_SERVER, __uuidof(ICalculator), (void**)&pCalc);
 
-    if (SUCCEEDED(hr)) 
+    if (FAILED(hr))
     {
-        std::cout << "x = " << x << std::endl;
-        std::cout << "y = " << y << std::endl;
-
-        hr = pCalc->Add(x, y, &result);
-        std::cout << "x + y = " << result << std::endl;
-        
-        hr = pCalc->Subtract(x, y, &result);
-        std::cout << "x - y = " << result << std::endl;
-
-        // Create the event sink.
-        CComPtr<IDispatch> pEventSink = new EventSink();
-
-        // Connect the event sink to the COM object.
-        CComQIPtr<IConnectionPointContainer> pCPC(pCalc);
-        CComPtr<IConnectionPoint> pCP;
-        hr = pCPC->FindConnectionPoint(__uuidof(ICalculatorEvents), &pCP);
-        if (SUCCEEDED(hr))
-        {
-            DWORD cookie;
-            // Connect the event sink.
-            pCP->Advise(pEventSink, &cookie);
-            // Trigger the multiply operation.
-            pCalc->Multiply(x, y);
-
-            std::string s;
-            std::cin >> s;
-
-            // Disconnect the event sink
-            pCP->Unadvise(cookie);
-        }
-        else
-        {
-            std::cerr << "Failed to connect event sink: " << std::system_category().message(hr) << std::endl;
-        }
+        return ExitWithError(hr, "Failed to create COM object: ");
     }
-    else
+
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
+
+    hr = pCalc->Add(x, y, &result);
+    std::cout << "x + y = " << result << std::endl;
+
+    hr = pCalc->Subtract(x, y, &result);
+    std::cout << "x - y = " << result << std::endl;
+
+    // Create the event sink.
+    CComPtr<IDispatch> pEventSink = new EventSink();
+
+    // Connect the event sink to the COM object.
+    CComQIPtr<IConnectionPointContainer> pCPC(pCalc);
+    CComPtr<IConnectionPoint> pCP;
+    hr = pCPC->FindConnectionPoint(__uuidof(ICalculatorEvents), &pCP);
+    if (FAILED(hr))
     {
-        std::cerr << "Failed to create COM object: " << std::system_category().message(hr) << std::endl;
+        return ExitWithError(hr, "Failed to connect event sink: ");
     }
+
+    DWORD cookie;
+    // Connect the event sink.
+    pCP->Advise(pEventSink, &cookie);
+    // Trigger the multiply operation.
+    pCalc->Multiply(x, y);
+
+    std::string s;
+    getline(std::cin, s);
+
+    // Disconnect the event sink
+    pCP->Unadvise(cookie);
 
     CoUninitialize();
+    return 0;
 }
